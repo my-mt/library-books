1dc38021a66010fa4c95a2a4d8ebbd842684d616
tihanin <tihanin_ne@mail.ru>
2018-07-08 18:53:48 +0300

    gitignore config

--- config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'devtesmail@devtes.ru', // автоматическая отправка почты с данного email
-    'secretKeyExpire' => 60 * 60 // время хранения секретного ключа
-];
